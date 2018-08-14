d254fad1ca7d877f077fa7bdfd0a3c0d74d36182
Fabian Dvorak <fabian.dvorak@uni.kn>
2018-08-14 17:14:30 +0200

    setup src

--- R/hello.R
+++ R/hello.R
@@ -1,6 +1,6 @@
 # Hello, world!
 #
-# This is an example function named 'hello' 
+# This is an example function named 'hello'
 # which prints 'Hello, world!'.
 #
 # You can learn more about package authoring with RStudio at:
@@ -13,6 +13,8 @@
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
 
+#' @useDynLib stratEst
+#' @importFrom Rcpp sourceCpp
 hello <- function() {
   print("Hello, world!")
 }
