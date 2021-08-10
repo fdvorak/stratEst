be28d91885ba79a040b20b38819f1fc8e362d84f
Fabian Dvorak <fabian.dvorak@uni.kn>
2021-08-10 22:31:34 +0200

    RcppExports.o

--- src-i386/RcppExports.cpp
+++ src-i386/RcppExports.cpp
@@ -6,6 +6,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // rcpparma_hello_world
 arma::mat rcpparma_hello_world();
 RcppExport SEXP _stratEst_rcpparma_hello_world() {
